2d8df70a8e7b23a31b5e3295c7cdc22c76fd9c04
GRIZA123 <mohamadbomroz2@gmail.com>
2025-02-19 17:37:06 +0100

    add icon in task bar

---
+++ forpc/utils/Encryption.py
@@ -0,0 +1,39 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+import hashlib
+import base64
+
+iv = b'\x10\x0c1\t\x9f\xb1\x10\x94D4\xcf\x99\x8en&\xbf'
+
+def encrypt(plaintext, key, iv=iv):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(algorithms.AES.block_size).padder()
+    padded_data = padder.update(plaintext.encode()) + padder.finalize()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+    return base64.b64encode(ciphertext).decode()
+
+def decrypt(ciphertext, key, iv=iv):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    padded_plaintext = decryptor.update(base64.b64decode(ciphertext)) + decryptor.finalize()
+    unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+    plaintext = unpadder.update(padded_plaintext) + unpadder.finalize()
+    return plaintext.decode()
+
+def simple_hash_256(input_string):
+    if not input_string:
+        raise ValueError("Input string for hashing cannot be empty!")
+    return hashlib.sha256(input_string.encode()).digest()[:32]
+
+
+if __name__ == "__main__":
+    key = simple_hash_256("password") 
+    plaintext = ""
+    
+    encrypted = encrypt(plaintext, key)
+    print("Encrypted:", encrypted)
+
+    decrypted = decrypt(encrypted, key)
+    print("Decrypted:", decrypted)
---
+++ forpc/utils/shared.py
@@ -0,0 +1,7 @@
+
+data_account = []
+name_account = ""
+account_delete = []
+name_of_account_delete = ""
+tlist = {}
+tray_icon = None 
